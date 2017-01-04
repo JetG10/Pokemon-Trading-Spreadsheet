281727d1e25c55bfb87390afd81ed0ecf1427ae4
JetG10 <jjg3096@gmail.com>
2017-01-03 19:51:50 -0500

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,29 +2,30 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1h8WjTiesswubhHbKRgafsVamIsB3q_VzqfND3WRxka4";
 
 // ***********************************************************************************
 // Position of Breedables, Shinies and Wants worksheet
 // ***********************************************************************************
 // If you don't have one worksheet, set the value to -1 to disable it
-var breedablesWorksheet = 1;
-var shiniesWorksheet = 2;
-var wantsWorksheet = 3;
+var breedjectsWorksheet = 1;
+var perfectsWorksheet = 2;
+var breedablesWorksheet = 3;
+var lookingforWorksheet = 4;
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "1478-7956-0204";
+var inGameName = "Jet";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/jet_10";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://65.media.tumblr.com/88c836d5d2e467f5e28e920e072621eb/tumblr_inline_o8qe4fjBmK1skcxt0_540.png";
